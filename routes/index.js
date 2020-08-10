9a385591b66a6b7708f8e8bbc0a517baefbfe7f0
abood1996 <34720227+abood1996@users.noreply.github.com>
2020-08-10 14:42:46 +0300

    Update index.js
    MB Added

--- routes/index.js
+++ routes/index.js
@@ -119,15 +119,18 @@ router.get('/material', async (req, res, next) => {
         try {
             const pool = await poolPromise
             const queryResult = await pool.request()
-                .query('SELECT * FROM [' + DBName + '].[dbo].[MATERIALS] WHERE pos_icon = 1')
+                .input('Store_no', sql.Int, StoreNo)
+                .query('SELECT *, [' + DBName + '].[dbo].material_jard (M_NO,getdate(),@Store_no) As MB FROM [' + DBName + '].[dbo].[MATERIALS] WHERE pos_icon = 1')
 
             var reee = {
                 material: []
             };
             if (queryResult.recordset.length > 0) {
+              
+                    
+
                 queryResult.recordset.forEach(item => {
-                   
-                       
+               
                     reee.material.push({
                         M_NO: item.M_NO,
                         M_NAME_AR: item.M_NAME_AR,
@@ -137,7 +140,7 @@ router.get('/material', async (req, res, next) => {
                         PRICE_LIMIT: item.PRICE_LIMIT,
                         M_TAX: item.M_TAX,
                         M_GROUP: item.M_GROUP,
-                        material_balance: CalculateMaterialBalance(pool, item.M_NO, StoreNo, DBName, SName, UName, Pass),
+                        material_balance: item.MB,
                         M_PIC: item.m_pic
                     })
                 });
@@ -198,7 +201,8 @@ router.get('/materialByclass', async (req, res, next) => {
             const pool = await poolPromise
             const queryResult = await pool.request()
                 .input('CLASS_NO', sql.Decimal, ClassNo)
-                .query('SELECT * FROM [' + DBName + '].[dbo].[MATERIALS] WHERE CLASS_NO = @CLASS_NO AND pos_icon = 1')
+                .input('StoreNo', sql.Decimal, StoreNo)
+                .query('SELECT *, [' + DBName + '].[dbo].material_jard (M_NO,getdate(),@StoreNo) As MB FROM [' + DBName + '].[dbo].[MATERIALS] WHERE CLASS_NO = @CLASS_NO AND pos_icon = 1')
 
             var reee = {
                 material: []
@@ -216,7 +220,7 @@ router.get('/materialByclass', async (req, res, next) => {
                         PRICE_LIMIT: item.PRICE_LIMIT,
                         M_TAX: item.M_TAX,
                         M_GROUP: item.M_GROUP,
-                        material_balance: CalculateMaterialBalance(pool, item.M_NO, StoreNo, DBName, SName, UName, Pass),
+                        material_balance: item.MB,
                         M_PIC: item.m_pic
                     })
                 });
@@ -232,127 +236,68 @@ router.get('/materialByclass', async (req, res, next) => {
     }
 
 });
-//Calculate Materila Balance
-function CalculateMaterialBalance(pool,M_NO, Store_no, DBName, SName, UName, Pass) {
-   
-    console.log("M_NO = " + M_NO)
-    console.log("Store_no = " + Store_no)
-    console.log("DBName = " + DBName)
-    console.log("SName = " + SName)
-    console.log("UName = " + UName)
-    console.log("Pass = " + Pass)
-    const sql = require('mssql')
+//Get Materila Balance
+router.get('/materialBalance', async (req, res, next) => {
+    console.log(req.query);
 
-    if (Store_no == 0) {
-        return 999999
-    } else {
-        if (DBName != null && SName != null && UName != null && Pass != null) {
-            try {
+    var DBName = req.query.db
+    var SName = req.query.sname
+    var UName = req.query.uname
+    var Pass = req.query.pass
+    var StoreNo = req.query.store
+    var M_NO = req.query.mno
 
-                const entrance_detail = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT  SUM([m_quant]) AS Result FROM  [' + DBName + '].[ACC].[entrance_detail] Where [M_NO] =@M_NO AND INVOICE_NO in (SELECT INVOICE_NO FROM [' + DBName + '].[ACC].[entrance] WHERE [store_no] =@Store_no)')
+    var config = {
 
-                const move_out_detail = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[move_out_detail] Where[M_NO] = @M_NO AND INVOICE_NO in (SELECT INVOICE_NO FROM [' + DBName + '].[ACC].[move_out] WHERE [store_no] =@Store_no)')
-
-                const damage_detail = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[damage_detail] Where[M_NO] = @M_NO AND INVOICE_NO in (SELECT INVOICE_NO FROM [' + DBName + '].[ACC].[damage] WHERE [store_no] =@Store_no)')
-
-
-                const sales_main_detail = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[sales_main_detail] Where[M_NO] = @M_NO AND INVOICE_NO in (SELECT INVOICE_NO FROM [' + DBName + '].[ACC].[SALES_main] WHERE [store_no] =@Store_no)')
-
-                const sales_r_main_detail = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[sales_r_main_detail] Where[M_NO] = @M_NO AND INVOICE_NO in (SELECT INVOICE_NO FROM [' + DBName + '].[ACC].[SALES_r_main] WHERE [store_no] =@Store_no)')
-
-                const purchases_main_detail = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[purchases_main_detail] Where[M_NO] = @M_NO AND [store_no] =@Store_no')
-
-                const purchases_r_main_detail = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[purchases_r_main_detail] Where[M_NO] = @M_NO AND [store_no]=@Store_no')
-
-                const remove_detailFrom = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[remove_detail] Where[M_NO] = @M_NO AND INVOICE_NO in (SELECT INVOICE_NO FROM [' + DBName + '].[ACC].[remove] WHERE [store_no_from] =@Store_no)')
-
-                const remove_detailTo = pool.request()
-                    .input('M_NO', sql.Int, M_NO)
-                    .input('Store_no', sql.Int, Store_no)
-                    .query('SELECT SUM([m_quant]) AS Result FROM [' + DBName + '].[ACC].[remove_detail] Where[M_NO] = @M_NO AND INVOICE_NO in (SELECT INVOICE_NO FROM [' + DBName + '].[ACC].[remove] WHERE [store_no_to] =@Store_no)')
-
-                var entrance = 0;
-                var move_out = 0;
-                var damage = 0;
-                var sales_main = 0;
-                var sales_r_main = 0;
-                var purchases_main = 0;
-                var purchases_r_main = 0;
-                var sToreFrom = 0;
-                var sToreTo = 0;
-
-                if (entrance_detail.recordset.length > 0 && entrance_detail.recordset[0].Result != null) {
-                    entrance = entrance_detail.recordset[0].Result
-                } if (move_out_detail.recordset.length > 0 && move_out_detail.recordset[0].Result != null) {
-                    move_out = move_out_detail.recordset[0].Result
-                } if (damage_detail.recordset.length > 0 && damage_detail.recordset[0].Result != null) {
-                    damage = damage_detail.recordset[0].Result
-                } if (sales_main_detail.recordset.length > 0 && sales_main_detail.recordset[0].Result != null) {
-                    sales_main = sales_main_detail.recordset[0].Result
-                } if (sales_r_main_detail.recordset.length > 0 && sales_r_main_detail.recordset[0].Result != null) {
-                    sales_r_main = sales_r_main_detail.recordset[0].Result
-                } if (purchases_main_detail.recordset.length > 0 && purchases_main_detail.recordset[0].Result != null) {
-                    purchases_main = purchases_main_detail.recordset[0].Result
-                } if (purchases_r_main_detail.recordset.length > 0 && purchases_r_main_detail.recordset[0].Result != null) {
-                    purchases_r_main = purchases_r_main_detail.recordset[0].Result
-                } if (remove_detailFrom.recordset.length > 0 && remove_detailFrom.recordset[0].Result != null) {
-                    sToreFrom = remove_detailFrom.recordset[0].Result
-                } if (remove_detailTo.recordset.length > 0 && remove_detailTo.recordset[0].Result != null) {
-                    sToreTo = remove_detailTo.recordset[0].Result
-                }
-
-                var sum = 0;
-                sum -= sales_main;
-                sum += sales_r_main;
-                sum += purchases_main;
-                sum -= purchases_r_main;
-                sum += entrance;
-                sum -= move_out;
-                sum -= damage;
-                sum -= sToreFrom;
-                sum += sToreTo;
-
-                console.log("SUM = " + sum)
-                return sum
-
-                //res.send(JSON.stringify({ success: true, result: sum }));
-
-            } catch (err) {
-                console.log(err.message)
-                //res.send(JSON.stringify({ success: false, message: err.message }))
-                return 0
+        server: SName,
+        authentication: {
+            type: 'default',
+            options: {
+                userName: UName,
+                password: Pass
             }
-        } else {
-            return 0
-            console.log("BAD CONNECTION")
-            //res.send(JSON.stringify({ success: false, message: "BAD CONNECTION" }))
+        },
+        options: {
+            database: DBName,
+            rowCollectionOnDone: true,
+            encrypt: false,
+
+            useColumnNames: false
         }
     }
-};
+
+
+    const poolPromise = new sql.ConnectionPool(config)
+        .connect()
+        .then(pool => {
+            console.log('Connected To MSSQL ' + config.database);
+            return pool;
+        }).catch(err => console.log('Database Connection Failed ! Bad Config :', err));
+
+
+
+    if (DBName != null && SName != null && UName != null && Pass != null) {
+        try {
+            const pool = await poolPromise
+            const queryResult = await pool.request()
+                .input('M_NO', sql.Decimal, M_NO)
+                .input('StoreNo', sql.Decimal, StoreNo)
+                .query('SELECT [' + DBName + '].[dbo].material_jard (@M_NO,getdate(),@StoreNo) As MB')
+
+           
+            if (queryResult.recordset.length > 0) {
+                res.send({ isSuccess: true, MB: queryResult.recordset[0].MB });
+            } else {
+                res.send(JSON.stringify({ isSuccess: false, msg: "Empity" }))
+            }
+        } catch (err) {
+            res.send(JSON.stringify({ isSuccess: false, msg: err.message }))
+        }
+    } else {
+        res.send(JSON.stringify({ isSuccess: false, msg: "BAD CONNECTION" }))
+    }
+
+});
 //================================================
 // Category
 // GET /  
@@ -851,7 +796,7 @@ router.post('/updateOrder', async (req, res, next) => {
                     order_detail[i]["machine_name_d"],
                     order_detail[i]["m_price_before"],
                     order_detail[i]["m_tot_before"],
-                    order_detail[i]["no_group"],
+                    order_detail[i]["no_group"]
                 )
             }
             const request = pool.request()
